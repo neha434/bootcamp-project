4237362a749d6984006d21f49d29a0dfbff93cab
neha.rai <neha.rai@tothenew.com>
2020-04-28 10:36:47 +0530

    schedular

--- project/ecommerce/src/main/java/com/springboot/ecommerceApplication/EcommerceApplication.java
+++ project/ecommerce/src/main/java/com/springboot/ecommerceApplication/EcommerceApplication.java
@@ -2,6 +2,7 @@ package com.springboot.ecommerceApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,8 +17,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-@EnableScheduling
 @SpringBootApplication
+@EnableScheduling
+@EnableJpaRepositories
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
--- project/ecommerce/src/main/java/com/springboot/ecommerceApplication/exception/AccountDoesNotExists.java
+++ project/ecommerce/src/main/java/com/springboot/ecommerceApplication/exception/AccountDoesNotExists.java
@@ -3,7 +3,7 @@ package com.springboot.ecommerceApplication.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CONFLICT)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class AccountDoesNotExists extends RuntimeException {
     public AccountDoesNotExists(String message){
         super(message);
